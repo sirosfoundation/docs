412147c54bc7f5457182d2fa201ab8ae7760f3c1
Simon <contact@smn.codes>
2025-11-27 13:15:02 +0100

    Update hero buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,8 +22,13 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Get started
+            to="/docs/sirosid">
+            SIROS ID
+          </Link>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/wwwallet">
+            wwWallet
           </Link>
         </div>
       </div>
