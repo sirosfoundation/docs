827da25308fff4ef583726dd093198a2d34da843
Simon <contact@smn.codes>
2025-12-09 16:24:29 +0100

    chore: update url and baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,10 +16,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://developer.siros.org',
+  url: 'https://sirosfoundation.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
